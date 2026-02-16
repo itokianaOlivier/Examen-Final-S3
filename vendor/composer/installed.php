6fa51e427628a9f0f25ffcad79da1df19e3af432
6lovatiana <upstarfearless@gmail.com>
2026-02-16 14:06:04 +0300

    feat: add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dbe01a68cecbae7826facafb237de28340cb00d3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dbe01a68cecbae7826facafb237de28340cb00d3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'flightphp/core' => array(
+            'pretty_version' => 'v3.18.0',
+            'version' => '3.18.0.0',
+            'reference' => '88d7032928c09b98e65ccd87b7f8a341a40cc3d2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../flightphp/core',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mikecao/flight' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.0.2',
+            ),
+        ),
+    ),
+);
